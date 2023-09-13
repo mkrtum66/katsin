d2f3e4f1a6f567c95a721c2b313c2976016a0bed
iMac <imac@iMacs-iMac.local>
2023-09-13 15:38:40 -0700

    changes

--- src/pages/eventsPage/eventsPage.jsx
+++ src/pages/eventsPage/eventsPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './eventsPage.scss';
+import cocktail from '../../assets/images/cocktail.jpg';
 
 const EventsPage = () => {
   return (
     <div className="eventsPage pageWrapper">
-      <div className="container">Events Page</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="mb-5">Currently we don't have upcoming events!</h1>
+          </div>
+          <div className="col-md-6">
+            <img src={cocktail} alt="img" className="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
